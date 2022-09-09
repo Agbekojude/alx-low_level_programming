b8205f251ce8004c50438d5413fe33de33906c60
Agbeko Jude <agbekojude@gmail.com>
2022-09-08 23:40:28 -0700

    task 5 correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - entry piont of C code
+ * main - entry point
  *
- * Return: result (succes)
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);
+	return (0) ;
 }
