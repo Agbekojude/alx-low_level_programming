99849ea9b5cef92ec41a2203908a2fd1d4c831bf
Agbeko Jude <agbekojude@gmail.com>
2022-09-08 21:46:09 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry piont of C code
+ *
+ * Return: result (succes)
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
