de970c725c95ae9475871b337cf7d84a1fed3dd6
Agbeko Jude <agbekojude@gmail.com>
2022-09-10 21:25:22 -0700

    task 4 correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print my low level 
+ * main -entry point
  *
- * Return: results
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
