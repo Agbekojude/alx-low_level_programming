08d8b0b8dd5a496afacf2baf87bfd49bdd1f216d
Agbeko Jude <agbekojude@gmail.com>
2022-09-12 21:12:43 -0700

    task 2 correct

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,7 +2,7 @@
 
 /**
  *
- * main - Prints the alphabet.
+ * main - Prints the alphabet
  * Return: Always 0 (Sucess)
  */
 
@@ -14,7 +14,7 @@ int main(void)
 			
 			for (i = 0; i < 26; i++)
 					{
-						putchar(alp[i])
+						putchar(alp[i]);
 					}
 					putchar('\n');
 					return (0);
