4893be38a04823d3543b769bbc66cd0b33bd1460
Agbeko Jude <agbekojude@gmail.com>
2022-09-12 21:37:58 -0700

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in upper and lower
+ *
+ * Return: zero
+ */
+
+int main(void)
+{
+	char alpha, ALPHA;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+	for (ALPHA = 'A'; ALPHA <= 'Z'; ALPHA++)
+		putchar(ALPHA);
+	putchar('\n');
+	return (0);
+}
