a9aee71bfd35b8b07cedbf969ff384b77dab72e3
Agbeko Jude <agbekojude@gmail.com>
2022-09-14 06:37:27 -0700

    task 0 fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - prints putchar
+ * main - putchar
+ *
  * Return: Always 0 (Success)
  */
 
 int main(void)
-
 {
 	_putchar('_');
 	_putchar('p');
@@ -17,6 +17,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
+
 }
