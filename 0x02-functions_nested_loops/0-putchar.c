42f0bdc57a519de07f4426c7b4b81dc6ee9ef2b4
Agbeko Jude <agbekojude@gmail.com>
2022-09-13 21:15:47 -0700

    task 0 fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
  * main - print out putchar
  *
  * Description: Uses the main header file
  * Return: Always 0
  */
+
 int main(void)
 {
 	_putchar('_');
+	_putchar('p');
 	_putchar('u');
 	_putchar('t');
 	_putchar('c');
