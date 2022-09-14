569c44a0dba71d0b212a806086c791cb8a47291f
Agbeko Jude <agbekojude@gmail.com>
2022-09-14 13:17:23 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints all the alphabets a-z
+ * Return: always 0 success
+ */
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <='z')
+	{
+		_putchar((int) alpha);
+		alpha++;
+	}
+	_putchar ((char) 10);
+}
