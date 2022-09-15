ea219ce63612c16a3edca8b23883f3aab377335c
Agbeko Jude <agbekojude@gmail.com>
2022-09-14 19:54:55 -0700

    task 2 completed

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabeti_x10-prints all the alphabets, a-z ten times 
+ *
+ * Return: Always 0 success
+ */
+
+void print_aphabet_x10(void)
+{
+	char alpha;
+	int index;
+
+	alpha = 'a'
+		for (index = 10; index++
+	{
+		while (alpha <= 'z')
+		{
+		_putchar ((int) alpha);
+		alpha++;
+		}
+
+	}
+		_putchar((char) 10);	
+		
+		}
