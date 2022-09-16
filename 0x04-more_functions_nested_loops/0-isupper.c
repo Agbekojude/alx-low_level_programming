edbaff9fc1dd9b6b5c8bf9290ce5eb40aa39e318
Agbeko Jude <agbekojude@gmail.com>
2022-09-15 18:35:29 -0700

    task 0 corrected

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,17 +3,19 @@
 /**
  * _isupper - checks if a character is uppercase or not
  * @c: character to be tested
+ *
  * Return: 1whether it is, 0 otherwise
  */
 
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 'A' && c <= 'Z')
 
 			{
 			return (1);
 			}
-
+	else
+	{
 			return(0);
-}
-			
+	}
+}			
