78c20d52f92cadbf5e2371408b23d7b146876275
Agbeko Jude <agbekojude@gmail.com>
2022-09-16 19:58:45 -0700

    task 2 completed

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isupper - check for uppercase 
- * @c: an integer  paramer 
+ * _isupper - check for uppercase
+ * @c: an integer  paramer
  *
  * Return: somthing but success
  */
-int _isupper(int c) 
+int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
 }
