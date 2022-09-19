137fc5d3509f8352a2b5e5ed5391ac14d490502a
Agbeko Jude <agbekojude@gmail.com>
2022-09-19 13:53:34 -0700

    task 2 done

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - return the lenght of string 
+ * @s: String
+ * Return: returns lenght as string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+	while (*(s + len) ! = '\0')
+		len++;
+	return (len);
+}
