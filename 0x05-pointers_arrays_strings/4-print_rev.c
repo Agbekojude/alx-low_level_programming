2eddf7f0352cc478b28a0e8897b353b25118774f
Agbeko Jude <agbekojude@gmail.com>
2022-09-19 14:54:32 -0700

    task4 done

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse way
+ * @s: string to reverse
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = strlen(s);
+
+	while (len --)
+		putchar(*(s + len));
+
+	putchar(10);
+}
