0ce5656ed573d08e842f3b64a91b679d22f463ec
Agbeko Jude <agbekojude@gmail.com>
2022-09-20 20:20:04 -0700

    task 6 string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character
+ * @str: string
+ *
+ * Return: nothing 
+ */
+
+void put2(char *str)
+{
+	int i;
+
+	for (i = 0; str [i] != '\0'; i++)
+	{
+		if (i % 2 ==0)
+			_putchar(str[i]);
+	}
+	_putchat('\n')
+}
+
