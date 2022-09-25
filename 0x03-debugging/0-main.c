101b49cbbab62b6334e97c5f504b6a98483b6544
Agbeko Jude <agbekojude@gmail.com>
2022-09-24 22:40:07 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * main - test function that prints if an iteger is positive or negative
+ * main - test function that prints if an integer is positive or negative
  * Return: 0
  */
 
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	i = 0
+	i = 0;
 		positive_or_negative(i);
 
 	return (0);
