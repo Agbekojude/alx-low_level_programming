1d9935c96d4de26531beb5b3678aa87a5fcf52cf
Agbeko Jude <agbekojude@gmail.com>
2022-09-25 20:57:28 -0700

    task 1 corrected

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	int i;
 
-	printf("infinte loop incoming :(\n");
+	printf("infinite loop incoming :(\n");
 
 	i = 0;
 	
@@ -19,7 +19,7 @@ int main(void)
 	 * 	putchar(i);
 	 * }
 	 */
-	printf("infinte loop avoided! \\0/\n");
+	printf("infinite loop avoided! \\0/\n");
 
 	return (0);
 }	
