b1645b17d52c4a1981050b219a8b133d28fc1e89
Agbeko Jude <agbekojude@gmail.com>
2022-09-28 21:55:45 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion -returns the lnght of a string
+ * @s: a string
+ *
+ * Return: lenght of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s)
+
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+	{
+		return (0);
+	}
+}
