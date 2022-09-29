4d1a43a6d81d0c64f4f2072d97caac5505d67b4a
Agbeko Jude <agbekojude@gmail.com>
2022-09-29 07:04:01 -0700

    task 4 corrected

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,16 +6,19 @@
  * @y: power
  *
  * Return: x to the power of y
- * -1 if x < 0
  */
 
 int _pow_recursion(int x, int y)
 {
+	if result = x;
+
 	if (y < 0)
 		return (-1);
-	if (y == 0)
-		return (0);
-	if (y == 1)
-		return (x);
-	return (x * _pow_recursion(x, y - 1));
+
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
 }
