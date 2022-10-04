db7910f2997bd6724e29357bb51fe1f3aea1f58b
Agbeko Jude <agbekojude@gmail.com>
2022-10-03 21:02:47 -0700

    advance task fixed

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
 
 
 /**
  * main - prints minimum number of coins to make change for an amount of money.
- * @argc: the argument count
+ * @args: the parameter entry
  * @argv: the argument vector
  *
  * Return: Always 0 if no error, else 1
  */
 
-int main(int argc, char *argv[])
+int main(int args, char *argv[])
 {
 	int a, b = 0, i, t;
 	int c[5] = {25, 10, 5, 2, 1};
 
-	if (argc != 2)
+	if (args != 2)
 
 	{
 		puts("Error");
