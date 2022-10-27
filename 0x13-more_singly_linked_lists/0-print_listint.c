ec443c52c2af1d29368304205d4ebf346b4c042f
Agbeko Jude <agbekojude@gmail.com>
2022-10-26 21:09:13 -0700

    task 0 corrected

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,16 +11,15 @@
 
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *cursor = h;
-	size_t count = 0;
+	size_t nodes = 0;
 
-	while (cursor != NULL)
+	while (h)
 	{
-		printf("%d\n", cursor->n);
-		count += 1;
+		nodes++;
 
-		cursor = cursor->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 
-	return (count);
+	return (nodes);
 }
