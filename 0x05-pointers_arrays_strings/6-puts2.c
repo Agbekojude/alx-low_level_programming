7a129a5d205e45f03237b64b8febdfc41e015efb
Agbekojude <agbekojude@gmail.com>
2023-08-22 03:22:52 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a
+ * string, starting with the first character
+ *
+ * @str: The used string reference
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
