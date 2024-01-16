265a5ae624620ea5f1184c7f67f051f881411314
Agbekojude <agbekojude@gmail.com>
2024-01-16 13:05:38 -0800

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+/**
+ * print_last_digit - Function that prints the last digit of a number
+ * @nld: umber's last digit result
+ * Return: Value of the last digit
+ */
+
+int print_last_digit(int nld)
+{
+	int pld;
+
+	pld = (nld % 10);
+
+	if (pld < 0)
+	{
+		pld = (-1 * pld);
+	}
+
+	_putchar(pld + '0');
+	return (pld);
+}
